d4efd56e021e9cf17bd0857010c2b3d7f80851a5
Leon <l84328g@gmail.com>
2022-07-20 03:05:39 +0800

    profit plot

--- my_backtest.py
+++ my_backtest.py
@@ -1,12 +1,21 @@
 import matplotlib.pyplot as plt
 from datetime import datetime
 from binance_trade_bot import backtest
+# TODO: stop condition
+# TODO: record more information
+# TODO: why getting slower?
+# TODO: compare with holding balance simply
+# TODO: check the big drop in this year
+# TODO: check the correctness of profit
 
 if __name__ == "__main__":
     history = []
     profit = []
     idx = 0
-    for manager in backtest(datetime(2022, 2, 1), datetime.now()):
+    start_time = datetime(2021, 1, 1)
+    init_time = start_time
+    start_month = start_time.month
+    for manager in backtest(start_time, datetime.now()):
         btc_value = manager.collate_coins("BTC")
         bridge_value = manager.collate_coins(manager.config.BRIDGE.symbol)
         history.append((btc_value, bridge_value))
@@ -21,10 +30,20 @@ if __name__ == "__main__":
         
         # TODO: start and end time
         profit.append(bridge_diff)
-        if idx % 200 == 0 and idx > 0:
-            plt.plot(profit)
-            plt.savefig(f'plot/ma/ma-USDT-{manager.datetime}.png')
+        # if bridge_diff < -40: #idx=593 923
+        #     print(bridge_diff)
+        print(idx)
+        if idx == 591:
+            print(3)
+        cur_month = manager.datetime.month
+        # if cur_month > start_month:
+        if idx%100==0 and idx>0:
+            plt.plot(profit[-100:])
+            # time_record = 
+            plt.savefig(
+                f'plot/ma/ma-USDT-{init_time.month}-{cur_month}-step{idx}.png')
             plt.show()
+            start_month = cur_month
         idx += 1
 
         with open('balance_log.txt', 'a+') as fw:
